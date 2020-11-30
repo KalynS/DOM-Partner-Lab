409457e770f399a8824db567f20abe04494ecdd9
Kalyn Smith <kalyn@Kalyns-MacBook-Air.local>
2020-11-30 16:35:40 -0500

    part one

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// let teamMatesTwo = document.getElementById("nameOne");
+// let teamMateTwo = document.getElementById("nameTwo");
+
+let userName = ["kalyn", " irma"];
+document.getElementById("teamMates").innerHTML = userName;
