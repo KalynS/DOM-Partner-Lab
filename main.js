b9ae2d8bbf3116df4aa7dd3bf911f87ec0e4ea6f
gglowe <gregory.g.lowe@gmail.com>
2020-12-01 16:24:53 -0500

    finished part one - part 2

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 document.getElementById("nameOne").innerHTML = "Kalyn";
 document.getElementById("nameTwo").innerHTML = "Irma";
+let logo = document.getElementById('logo')
 
-//let userName = ["kalyn", " irma"];
-//document.getElementById("teamMates").innerHTML = userName;
+const sizer = () => {
+
+let response = prompt("Please enter the value 'small' or 'large'")
+console.log(response)
+console.log(document.getElementById('logo'))
+
+if (response == "small") {
+    document.getElementById('logo').width=400
+    document.getElementById('logo').height=250
+}
+ else if (response == "large") {
+    document.getElementById('logo').width=800
+    document.getElementById('logo').height=500
+} else {
+    alert("Please refresh and enter 'small' or 'large'")
+}
+}
+
+sizer()
